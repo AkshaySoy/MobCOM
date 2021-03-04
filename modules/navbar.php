3a7d06c3003ae5e630ca1ab13977bcd4d63ec944
AkshaySoy <akshay.soy@gmail.com>
2021-03-04 15:06:07 +0530

    A small change

--- modules/navbar.php
+++ modules/navbar.php
@@ -58,6 +58,7 @@
                             <button class="btn btn-primary btn-block" type="button" data-toggle="modal" data-target="#exampleModalCenter">
                                 Login
                             </button>
+                            
                         </div>
                     </div>
                 </div>
